051fe935515643df2c7ecdc0b0b9c3f6aba75b06
iDgoe <e.novykov@gmail.com>
2023-03-30 23:26:02 +0300

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+// import { join } from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const paletteContainer = document.querySelector('.gallery');
+
+const cardsMarkup = createCards(galleryItems);
+
+function createCards(el) {
+  return el
+    .map(({ preview, original, description }) => {
+      return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
+paletteContainer.insertAdjacentHTML('afterbegin', cardsMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'buttom',
+  captionDelay: 250,
+});
+
+console.dir(lightbox);
